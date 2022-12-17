fea8328698694037540d5730d58be4ef2ea1644b
lawal toheeb damilare <lawaltd088@gmail.com>
2022-12-17 06:53:01 +0000

    monty push and pall function

--- monty_function1.c
+++ monty_function1.c
@@ -25,3 +25,24 @@ void monty_push(stack_t **stack, unsigned int number)
 	new->next = NULL;
 	tmp->next = new;
 }
+
+/**
+ * monty_pall - prints the values of a stack_t linked list
+ *
+ * @stack: pointer to the top node of a stack_t linked list
+ *
+ * @line_number: the current workinh line number of a monty bytecode file
+ *
+ */
+
+void monty_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = (*stack)->next;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	(void)line_number;
+}
